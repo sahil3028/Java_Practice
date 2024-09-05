d90f4d1d5b8ea356c0a8bd30f91c1a3ecff8ada3
Sahil <sahil12345work@gmail.com>
2024-09-05 17:19:59 +0530

    solving arraylist q1

--- Loops/src/arrayListQ1.java
+++ Loops/src/arrayListQ1.java
@@ -1,52 +1,63 @@
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Scanner;
 
 public class arrayListQ1 {
-    public static void main(String[] args) {
-        ArrayList<String> l1 = new ArrayList<>(List.of("a", "b", "c", "d"));
-        System.out.println(l1);
-        Scanner in = new Scanner(System.in);
-        String x;
 
-        while (true) {
-            System.out.println("1. add items\n2. remove items\n0. shutdown");
-            int choice = in.nextInt();
-            in.nextLine();
-            switch (choice) {
-                case 1:
-                    System.out.println("enter the element you wanna add : ");
-                    x = in.nextLine();
-                    while (l1.contains(x)) {
-                        System.out.println("Already Exists, Try again");
-                        x = in.nextLine();
-                    }
-                        l1.add(x);
-                        l1.sort(Comparator.naturalOrder());
-                        System.out.println(l1);
-                    break;
-                case 2:
-                    System.out.println("enter the element you wanna remove : ");
-                    x = in.nextLine();
-                    if (l1.contains(x)) {
-                        l1.remove(x);
-                        l1.sort(Comparator.naturalOrder());
-                        System.out.println(l1);
-                    }
-                    else{
-                        System.out.println("element is already missing");
-                    }
-                    break;
-                case 0:
-                    System.out.println("shutting down");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Wrong input, Try again");
-                    break;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        boolean flag = true;
+        ArrayList<String> groceries = new ArrayList<>();
+        while (flag) {
+            printActions();
+            switch (Integer.parseInt(scanner.nextLine())) {
+                case 1 -> addItems(groceries);
+                case 2 -> removeItems(groceries);
+                default -> flag = false;
+            }
+            groceries.sort(Comparator.naturalOrder());
+            System.out.println(groceries);
+        }
+    }
+
+    private static void addItems(ArrayList<String> groceries) {
+
+        System.out.println("Add item(s) [separate items by comma]:");
+        String[] items = scanner.nextLine().split(",");
+//        groceries.addAll(List.of(items));
+
+        for (String i: items) {
+            String trimmed = i.trim();
+            if (groceries.indexOf(trimmed) < 0) {
+                groceries.add(trimmed);
             }
         }
     }
-    }
 
+    private static void removeItems(ArrayList<String> groceries) {
+
+        System.out.println("Remove item(s) [separate items by comma]:");
+        String[] items = scanner.nextLine().split(",");
+
+        for (String i: items) {
+            String trimmed = i.trim();
+            groceries.remove(trimmed);
+        }
+    }
+    private static void printActions() {
+
+        String textBlock = """
+                Available actions:
+                                
+                0 - to shutdown
+                                
+                1 - to add item(s) to list (comma delimited list)
+                                
+                2 - to remove any items (comma delimited list)
+                                
+                Enter a number for which action you want to do:""";
+        System.out.print(textBlock + " ");
+    }
+}
