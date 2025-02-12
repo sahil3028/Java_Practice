041c9a5bafd68e3fc37187aa6c15f57150177bcb
Sahil <sahil12345work@gmail.com>
2025-02-13 00:01:03 +0530

    a linked list question (still a flow in adding)

--- Loops/src/LinkedListQ1.java
+++ Loops/src/LinkedListQ1.java
@@ -1,64 +1,94 @@
-import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Scanner;
+
+record Place(String name, int dist) {
+
+    @Override
+    public String toString() {
+        return String.format("reached %s at  %d km", name, dist);
+    }
+}
+
 public class LinkedListQ1 {
-    record store (String name, int distance){ }
 
-
-    public static void main(String[] args) {
-        LinkedList<testing.store> name = new LinkedList<>();
-        testing.store ax = new testing.store("x", 100);
-        testing.store bx = new testing.store("y", 200);
-        testing.store cx = new testing.store("z", 300);
-        testing.store dx = new testing.store("p", 400);
-        testing.store ex = new testing.store("q", 500);
-        testing.store fx = new testing.store("r", 600);
-
-        String printblock = """
-                 Choose the first letter or the word
-                 (F)orword
-                 (B)ackword
-                 (M)enu
-                 (Q)uit
-                
-                 """;
-
-        name.addAll(Arrays.asList(ax,bx,cx,dx,ex,fx));
-
-        var it= name.listIterator();
-
-
-        for(var i: name){
-            System.out.println(i);
+    public static void addPlace(LinkedList<Place> list, Place place) {
+        ListIterator<Place> itr = list.listIterator();
+        ListIterator<Place> dup = list.listIterator();
+        while(dup.hasNext()){
+            if(dup.next().name().equalsIgnoreCase(place.name())){
+                System.out.println("Duplicate found: " + place.name());
+                return;
+            }
         }
-        Scanner in = new Scanner(System.in);
-        System.out.println(printblock);
-        String option;
-        do {
-            System.out.println("enter choice");
-            option = in.nextLine();
-            switch (option) {
+
+        while (itr.hasNext()) {
+            Place current = itr.next();
+
+            if (current.dist() > place.dist()) {
+                itr.previous();
+                itr.add(place);
+                System.out.println("Added in order: " + place);
+                return;
+            }
+        }
+
+        // If no earlier insertion happened, add at the end
+        list.add(place);
+        System.out.println("Added at the end: " + place);
+    }
+    public static void printmenu(){
+        System.out.println("""
+                (F)orward
+                (B)ackwards
+                (A)dd a station
+                (Q)uit""");return;
+
+    }
+
+    public static void main(String[] args) { Scanner in=new Scanner(System.in);
+        LinkedList<Place> station = new LinkedList<>();
+
+        addPlace(station, new Place("Sydney", 0));
+        addPlace(station, new Place("Sydney", 0)); // Duplicate case
+        addPlace(station, new Place("Adelaide", 1374));
+        addPlace(station, new Place("Brisbane", 917));
+        addPlace(station, new Place("Perth", 3923));
+        addPlace(station, new Place("Alice Springs", 2771));
+        addPlace(station, new Place("Darwin", 3972));
+        addPlace(station, new Place("Melbourne", 877));
+
+        System.out.println("\nFinal Sorted List: " + station);
+        ListIterator<Place> move= station.listIterator();
+        while(true){
+            printmenu();
+            String c=in.next();
+            switch(c){
                 case "f":
-                case "forword":
-                    if (it.hasNext())
-                        System.out.println(it.next());
-                    else System.out.println("end of the station");
+                    if(move.hasNext())
+                    System.out.println(move.next());
+                    else System.out.println("end reached");
                     break;
                 case "b":
-                case "bakcword":
-                    if (it.hasPrevious())
-                        System.out.println(it.previous());
-                    else System.out.println("returned to the beinging of the station");
+                    if (move.hasPrevious())
+                    System.out.println(move.previous());
+                    else System.out.println("reached the start again");
                     break;
-                case "m":
-                case "menu":
-                    System.out.println(printblock);
+                case "a":
+                    System.out.println("enter station");
+                    String n= in.next();
+                    System.out.println("enter dist frm the start");
+                    int d= in.nextInt();
+                    addPlace(station,new Place(n,d));
                     break;
-                case "q":
-                case "quit":
-                    //System.exit(0);
-                    break;
+                case"q":
+                    System.out.println("leaving");
+                    return;
+
+                default:
+                    System.out.println("try again");
             }
-        } while (!option.equals("q"));
+        }
+
     }
 }
